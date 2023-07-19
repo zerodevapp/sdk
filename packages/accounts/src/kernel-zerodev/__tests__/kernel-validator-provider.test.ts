6e33fbb65473c8f21053fa9f01a0da3a7881abc8
Sahil Vasava <sahilvasava@gmail.com>
2023-07-19 02:31:35 +0200

    style: prettier fixes

--- packages/accounts/src/kernel-zerodev/__tests__/kernel-validator-provider.test.ts
+++ packages/accounts/src/kernel-zerodev/__tests__/kernel-validator-provider.test.ts
@@ -15,7 +15,7 @@ describe("Kernel Validator Provider Test", async () => {
 
   const client = createPublicClient({
     chain: polygonMumbai,
-    transport: http('https://rpc.ankr.com/polygon_mumbai'),
+    transport: http("https://rpc.ankr.com/polygon_mumbai"),
   });
 
   let accountAddress: Hex = "0x";
