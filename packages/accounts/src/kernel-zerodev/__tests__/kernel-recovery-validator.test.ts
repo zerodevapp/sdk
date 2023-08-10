ab7954508a8874b12a1911af59a86ce8ab6455e4
Archisman Das <dasarchisman25@gmail.com>
2023-08-11 00:39:56 +0530

    chore: userop test for recovery validator

--- packages/accounts/src/kernel-zerodev/__tests__/kernel-recovery-validator.test.ts
+++ packages/accounts/src/kernel-zerodev/__tests__/kernel-recovery-validator.test.ts
@@ -1,6 +1,6 @@
 import { LocalAccountSigner } from "@alchemy/aa-core";
 import { config } from "./kernel-account.test.js";
-import type { PrivateKeyAccount } from "viem";
+import type { Hash, PrivateKeyAccount } from "viem";
 import { SocialRecoveryValidator } from "../validator/social-recovery-validator.js";
 import { MockSigner } from "./mocks/mock-signer.js";
 import { SocialRecoveryProvider } from "../validator-provider/social-recovery-provider.js";
@@ -22,9 +22,7 @@ describe("Recovery Validator Test", async () => {
   });
 
   it("should return correct owner address", async () => {
-    expect(await validator.getOwner()).eql(
-      await mockOwner.getAddress()
-    );
+    expect(await validator.getOwner()).eql(await mockOwner.getAddress());
   });
 
   it("should sign hash properly", async () => {
@@ -40,4 +38,36 @@ describe("Recovery Validator Test", async () => {
       "0x4d61c5c27fb64b207cbf3bcf60d78e725659cff5f93db9a1316162117dff72aa631761619d93d4d97dfb761ba00b61f9274c6a4a76e494df644d968dd84ddcdb1c"
     );
   });
+
+  it(
+    "sendUserOperation should execute properly",
+    async () => {
+      let socialRecoveryProvider = await SocialRecoveryProvider.init({
+        projectId: config.projectId,
+        owner,
+        usePaymaster: false,
+        opts: {
+          providerConfig: {
+            opts: {
+              txMaxRetries: 10,
+            },
+          },
+        },
+      });
+
+      await socialRecoveryProvider.getAccount().getInitCode();
+      const result = socialRecoveryProvider.sendUserOperation({
+        target: "0x118aefa610ceb7c42c73d83dfc3d8c54124a4946",
+        data: "0x",
+        value: 0n,
+      });
+      await expect(result).resolves.not.toThrowError();
+      await socialRecoveryProvider.waitForUserOperationTransaction(
+        (
+          await result
+        ).hash as Hash
+      );
+    },
+    { timeout: 100000 }
+  );
 });
