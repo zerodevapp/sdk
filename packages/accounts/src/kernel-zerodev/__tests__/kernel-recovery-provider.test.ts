e1dbaf3e1688fef64357f9824b84d64f02979060
Archisman Das <dasarchisman25@gmail.com>
2023-08-24 18:56:22 +0530

    fix: recovery provider tests

--- packages/accounts/src/kernel-zerodev/__tests__/kernel-recovery-provider.test.ts
+++ packages/accounts/src/kernel-zerodev/__tests__/kernel-recovery-provider.test.ts
@@ -4,13 +4,16 @@ import { SocialRecoveryValidatorAbi } from "../abis/SocialRecoveryValidatorAbi.j
 import { config } from "./kernel-account.test.js";
 import { LocalAccountSigner } from "@alchemy/aa-core";
 import { generatePrivateKey } from "viem/accounts";
-import { SocialRecoveryProvider } from "../validator-provider/index.js";
+import { ECDSAProvider, SocialRecoveryProvider } from "../validator-provider/index.js";
+import { ValidatorMode } from "../validator/base.js";
 
 describe("Kernel Recovery Provider Test", async () => {
   const owner = LocalAccountSigner.privateKeyToAccountSigner(config.privateKey);
   const secondOwner = LocalAccountSigner.privateKeyToAccountSigner(
     generatePrivateKey()
   );
+
+  const ownerAddress = await owner.getAddress();
 
   const client = createPublicClient({
     chain: polygonMumbai,
@@ -19,32 +22,64 @@ describe("Kernel Recovery Provider Test", async () => {
 
   let accountAddress: Hex = "0x";
 
-  it("should add guardians correctly", async () => {
-    const socialRecoveryProvider = await SocialRecoveryProvider.init({
-      projectId: "c73037ef-8c0b-48be-a581-1f3d161151d3",
-      owner,
-      opts: {
-        accountConfig: {
-          index: 10050n,
-        },
-        providerConfig: {
-          opts: {
-            txMaxRetries: 10,
-            txRetryIntervalMs: 2000,
+  let ecdsaProvider:ECDSAProvider;
+  let socialRecoveryProvider:SocialRecoveryProvider;
+
+  beforeAll(async () => {
+    ecdsaProvider = await ECDSAProvider.init({
+        projectId: config.projectIdWithGasSponsorship,
+        owner,
+        opts: {
+          accountConfig: {
+            index: 70001n,
+          },
+          paymasterConfig: {
+            policy: "VERIFYING_PAYMASTER",
           },
         },
-        paymasterConfig: {
-          policy: "VERIFYING_PAYMASTER",
-        },
-      },
-    });
+      });
+      let accountAddress = await ecdsaProvider.getAccount().getAddress();
+      console.log("accountAddress", accountAddress);
 
+      if (!(await ecdsaProvider.getAccount().isAccountDeployed())) {
+        const depResult = await ecdsaProvider.sendUserOperation({
+          target: await owner.getAddress(),
+          data: "0x",
+        });
+        console.log("depResult", depResult);
+        await ecdsaProvider.waitForUserOperationTransaction(
+          depResult.hash as Hex
+        );
+      }
+
+    socialRecoveryProvider = await SocialRecoveryProvider.init({
+        projectId: config.projectIdWithGasSponsorship,
+        owner,
+        usePaymaster: true,
+        opts: {
+          providerConfig: {
+            opts: {
+              txMaxRetries: 10,
+            },
+          },
+          accountConfig:{
+            accountAddress
+          },
+          validatorConfig:{
+            mode:ValidatorMode.sudo,
+            validatorAddress:"0x85DD0a0cD046ffC2e72DcD8CD559FaA0bE94c4CC",
+          }
+        },
+      });
+  });
+
+  it("should add guardians correctly", async () => {
     const data = {
         guardians: {
           "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4": 100,
         },
         threshold: 50,
-        owneraddress: "0x7c8999dc9a822c1f0df42023113edb4fdd543266",
+        owneraddress: ownerAddress,
       };
 
     const res = await socialRecoveryProvider.setGuardians(data);
@@ -53,30 +88,21 @@ describe("Kernel Recovery Provider Test", async () => {
   },{timeout: 100000});
 
   it("should add recovery message and new owner", async()=>{
-    const socialRecoveryProvider = await SocialRecoveryProvider.init({
-      projectId: "c73037ef-8c0b-48be-a581-1f3d161151d3",
-      owner,
-      opts: {
-        accountConfig: {
-          index: 10050n,
-        },
-        providerConfig: {
-          opts: {
-            txMaxRetries: 10,
-            txRetryIntervalMs: 2000,
-          },
-        },
-        paymasterConfig: {
-          policy: "VERIFYING_PAYMASTER",
-        },
-      },
-    });
 
     const res = await socialRecoveryProvider.initRecovery(
-        "0x7c8999dc9a822c1f0df42023113edb4fdd543266",
+        ownerAddress,
         "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
     ) 
 
     console.log(res)
-  },{timeout: 100000})
+  },{timeout: 100000});
+
+  it("should recover account", async()=>{
+    const calldata = '0x011e732281b5dc6815686f4329be03d89d57a92ee3d0bc38d29659e7b2164ec6d3018e0822baf9fced11f96b2c1f41a547b4794bd1659360cafd388cdf26c463b91c1e732281b5dc6815686f4329be03d89d57a92ee3d0bc38d29659e7b2164ec6d3018e0822baf9fced11f96b2c1f41a547b4794bd1659360cafd388cdf26c463b91c1e732281b5dc6815686f4329be03d89d57a92ee3d0bc38d29659e7b2164ec6d3018e0822baf9fced11f96b2c1f41a547b4794bd1659360cafd388cdf26c463b91c1e732281b5dc6815686f4329be03d89d57a92ee3d0bc38d29659e7b2164ec6d3018e0822baf9fced11f96b2c1f41a547b4794bd1659360cafd388cdf26c463b91c'
+    const res = await socialRecoveryProvider.submitRecovery(
+        calldata
+    );
+
+    console.log(res)
+  },{timeout: 100000});
 });
