9f99e796e0be44e0b9c295fe14a51652a29d618d
Sahil Vasava <sahilvasava@gmail.com>
2023-10-11 09:21:24 +0900

    refactor: simplified rules params in session key

--- packages/accounts/src/kernel-zerodev/validator/types.ts
+++ packages/accounts/src/kernel-zerodev/validator/types.ts
@@ -1,3 +1,11 @@
+import {
+  type AbiParameter,
+  type AbiParameterKind,
+  type AbiParameterToPrimitiveType,
+  type AbiFunction,
+  type ExtractAbiFunction,
+} from "abitype";
+import { type Pretty } from "abitype/src/types.js";
 import type {
   ECDSAValidator,
   ECDSAValidatorParams,
@@ -22,7 +30,9 @@ import type {
   Permission,
   SessionKeyData,
   SessionKeyParams,
+  ParamCondition,
 } from "./session-key-validator.js";
+import type { Abi, InferFunctionName, Narrow } from "viem";
 
 export type SupportedValidators =
   | "ECDSA"
@@ -56,3 +66,57 @@ export type ValidatorMap = {
 export type EthereumProvider = { request(...args: any): Promise<any> };
 
 export type { ParamRules, Permission, SessionKeyData, SessionKeyParams };
+
+// Session Key types
+export type AbiParametersToPrimitiveTypes<
+  TAbiParameters extends readonly AbiParameter[],
+  TAbiParameterKind extends AbiParameterKind = AbiParameterKind
+> = Pretty<{
+  [K in keyof TAbiParameters]: AbiParameterToPrimitiveType<
+    TAbiParameters[K],
+    TAbiParameterKind
+  > | null;
+}>;
+
+export type AbiParametersToConditons<
+  TAbiParameters extends readonly AbiParameter[]
+> = Pretty<{
+  [K in keyof TAbiParameters]: ParamCondition | null;
+}>;
+
+export type GetFunctionArgs<
+  TAbi extends Abi | readonly unknown[],
+  TFunctionName extends string,
+  TAbiFunction extends AbiFunction = TAbi extends Abi
+    ? ExtractAbiFunction<TAbi, TFunctionName>
+    : AbiFunction,
+  TArgs = AbiParametersToPrimitiveTypes<TAbiFunction["inputs"]>,
+  TConditions = AbiParametersToConditons<TAbiFunction["inputs"]>,
+  FailedToParseArgs =
+    | ([TArgs] extends [never] ? true : false)
+    | (readonly unknown[] extends TArgs ? true : false)
+> = true extends FailedToParseArgs
+  ? {
+      args?: readonly unknown[];
+      conditions?: readonly unknown[];
+    }
+  : TArgs extends readonly []
+  ? { args?: never; conditions?: never }
+  : {
+      args?: TArgs;
+      conditions?: TConditions;
+    };
+
+export type GeneratePermissionFromArgsParameters<
+  TAbi extends Abi | readonly unknown[],
+  TFunctionName extends string | undefined = string,
+  _FunctionName = TAbi extends Abi
+    ? InferFunctionName<TAbi, TFunctionName>
+    : never
+> = Pick<Permission, "operation" | "target" | "valueLimit"> & {
+  functionName?: _FunctionName;
+} & (TFunctionName extends string
+    ? { abi: Narrow<TAbi> } & GetFunctionArgs<TAbi, TFunctionName>
+    : _FunctionName extends string
+    ? { abi: [Narrow<TAbi[number]>] } & GetFunctionArgs<TAbi, _FunctionName>
+    : never);
