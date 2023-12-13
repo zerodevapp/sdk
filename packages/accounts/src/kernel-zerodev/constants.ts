8174271b18a955c3774a7224e5d3d34da161fbc4
Blake Duncan <blakecduncan@gmail.com>
2023-12-13 14:34:29 -0500

    chore: add more sepolia support

--- packages/accounts/src/kernel-zerodev/constants.ts
+++ packages/accounts/src/kernel-zerodev/constants.ts
@@ -106,5 +106,8 @@ export const CHAIN_ID_TO_NODE: { [key: number]: string } = {
   204: "https://opbnb.rpc.thirdweb.com/eed98c1f30120c0508f17c1f01d3102b",
   42069: "https://rpc.op-testnet.gelato.digital",
   1261120: "https://rpc.zkatana.gelato.digital",
+  84532: `https://base-sepolia.infura.io/v3/${INFURA_API_KEY}`,
+  421614: `https://arbitrum-sepolia.infura.io/v3/${INFURA_API_KEY}`,
+  11155420: `https://optimism-sepolia.infura.io/v3/${INFURA_API_KEY}`,
   // 5611: 'opBnb-testnet',
 };
