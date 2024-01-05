f4d4872b6da5a780c07dad7db9376dfe9d095675
Derek Chiang <me@derekchiang.com>
2024-01-04 19:02:52 -0500

    Clean up code comments

--- packages/core/actions/kernel/sponsorUserOperation.ts
+++ packages/core/actions/kernel/sponsorUserOperation.ts
@@ -36,28 +36,6 @@ export type SponsorUserOperationReturnType = UserOperation
 
 /**
  * Returns paymasterAndData & updated gas parameters required to sponsor a userOperation.
- *
- * - Docs: https://docs.pimlico.io/permissionless/reference/stackup-paymaster-actions/sponsorUserOperation
- *
- * @param client {@link PimlicoBundlerClient} that you created using viem's createClient whose transport url is pointing to the Pimlico's bundler.
- * @param args {@link sponsorUserOperationParameters} UserOperation you want to sponsor & entryPoint.
- * @returns paymasterAndData & updated gas parameters, see {@link SponsorUserOperationReturnType}
- *
- *
- * @example
- * import { createClient } from "viem"
- * import { sponsorUserOperation } from "permissionless/actions/stackup"
- *
- * const bundlerClient = createClient({
- *      chain: goerli,
- *      transport: http("https://api.stackup.sh/v2/paymaster/YOUR_API_KEY_HERE")
- * })
- *
- * await sponsorUserOperation(bundlerClient, {
- *      userOperation: userOperationWithDummySignature,
- *      entryPoint: entryPoint
- * }})
- *
  */
 export const sponsorUserOperation = async (
     client: ZeroDevPaymasterClient,
--- packages/core/clients/decorators/kernel.ts
+++ packages/core/clients/decorators/kernel.ts
@@ -9,25 +9,6 @@ import type { ZeroDevPaymasterClient } from "../kernel"
 export type ZeroDevPaymasterClientActions = {
     /**
      * Returns paymasterAndData & updated gas parameters required to sponsor a userOperation.
-     *
-     *
-     * @param args {@link SponsorUserOperationParameters} UserOperation you want to sponsor & entryPoint.
-     * @returns paymasterAndData & updated gas parameters, see {@link SponsorUserOperationReturnType}
-     *
-     * @example
-     * import { createClient } from "viem"
-     * import { sponsorUserOperation } from "permissionless/actions/pimlico"
-     *
-     * const bundlerClient = createClient({
-     *      chain: goerli,
-     *      transport: http("https://api.pimlico.io/v2/goerli/rpc?apikey=YOUR_API_KEY_HERE")
-     * }).extend(pimlicoPaymasterActions)
-     *
-     * await bundlerClient.sponsorUserOperation(bundlerClient, {
-     *      userOperation: userOperationWithDummySignature,
-     *      entryPoint: entryPoint
-     * }})
-     *
      */
     sponsorUserOperation: (
         args: SponsorUserOperationParameters
