330b84c6a0a9ebecb226dac23787198b356374ac
Derek Chiang <me@derekchiang.com>
2024-01-19 17:31:42 -0500

    Export oneAddress

--- plugins/sessionKey/index.ts
+++ plugins/sessionKey/index.ts
@@ -10,3 +10,5 @@ export const SESSION_KEY_VALIDATOR_ADDRESS =
     "0x5C06CE2b673fD5E6e56076e40DD46aB67f5a72A5"
 export { serializeSessionKeyAccount } from "./serializeSessionKeyAccount.js"
 export { deserializeSessionKeyAccount } from "./deserializeSessionKeyAccount.js"
+
+export const oneAddress = "0x0000000000000000000000000000000000000001"
