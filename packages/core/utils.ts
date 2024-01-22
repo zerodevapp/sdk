22db98b4490d4c883ca1e6cb061e374bb797aec5
Blake Duncan <blakecduncan@gmail.com>
2024-01-22 18:30:46 -0500

    add util functions

--- packages/core/utils.ts
+++ packages/core/utils.ts
@@ -1,4 +1,12 @@
-import { type Address, zeroAddress } from "viem"
+import { walletClientToCustomSigner } from "permissionless"
+import {
+    type Address,
+    type Hex,
+    type WalletClient,
+    createWalletClient,
+    custom,
+    zeroAddress
+} from "viem"
 import { KernelImplToVersionMap, LATEST_KERNEL_VERSION } from "./constants.js"
 
 export const getKernelVersion = (kernelImpl?: Address): string => {
@@ -7,4 +15,27 @@ export const getKernelVersion = (kernelImpl?: Address): string => {
         if (addr.toLowerCase() === kernelImpl.toLowerCase()) return ver
     }
     return "0.2.1"
+}
+
+interface EIP1193Provider {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    request(args: { method: string; params?: Array<any> }): Promise<any>
+}
+
+export const providerToSmartAccountSigner = async (
+    provider: EIP1193Provider
+) => {
+    const [account] = await provider.request({ method: "eth_requestAccounts" })
+    const walletClient = createWalletClient({
+        account: account as Hex,
+        transport: custom(provider)
+    })
+    return walletClientToCustomSigner(walletClient)
+}
+
+export const walletClientToSmartAccountSigner = async (
+    walletClient: WalletClient
+) => {
+    // biome-ignore lint/suspicious/noExplicitAny: I believe this type error is because of a version mismatch between viem and permissionless
+    return walletClientToCustomSigner(walletClient as any)
 }
