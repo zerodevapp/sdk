30dcb473581a2b2902e92c4530330bbbb4d22146
adnpark <aidenp.dev@gmail.com>
2024-02-21 16:37:03 +0900

    feat: update contract address

--- plugins/passkey/index.ts
+++ plugins/passkey/index.ts
@@ -7,4 +7,4 @@ import {
 export { createPasskeyValidator, getPasskeyValidator, type KernelValidator }
 
 export const WEBAUTHN_VALIDATOR_ADDRESS =
-    "0x0Bc1C061878deAb416B2249D6009D07A72E367C3"
+    "0x82396bEb4cA5045EA82bc5553c5eFba0547Bea29"
