258a0517f28fc7805f7e49acb42499815b87eef6
adnpark <aidenp.dev@gmail.com>
2024-02-27 20:32:44 +0900

    chore: format

--- plugins/passkey/index.ts
+++ plugins/passkey/index.ts
@@ -1,10 +1,10 @@
 import type { KernelValidator } from "@zerodev/sdk/types"
 import {
-  createPasskeyValidator,
-  getPasskeyValidator,
+    createPasskeyValidator,
+    getPasskeyValidator
 } from "./toPasskeyValidator.js"
 
 export { createPasskeyValidator, getPasskeyValidator, type KernelValidator }
 
 export const WEBAUTHN_VALIDATOR_ADDRESS =
-  "0x3a0980D497F908fE5eaA5505af85C10911a718FD"
+    "0x3a0980D497F908fE5eaA5505af85C10911a718FD"
