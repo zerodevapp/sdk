78781b2124d58bcb59067c4067a67660e702551b
adnpark <aidenp.dev@gmail.com>
2024-02-27 20:32:23 +0900

    bump version

--- plugins/passkey/index.ts
+++ plugins/passkey/index.ts
@@ -1,10 +1,10 @@
 import type { KernelValidator } from "@zerodev/sdk/types"
 import {
-    createPasskeyValidator,
-    getPasskeyValidator
+  createPasskeyValidator,
+  getPasskeyValidator,
 } from "./toPasskeyValidator.js"
 
 export { createPasskeyValidator, getPasskeyValidator, type KernelValidator }
 
 export const WEBAUTHN_VALIDATOR_ADDRESS =
-    "0x01272b9d6c9321BD4146531c2874111E86e39f96"
+  "0x3a0980D497F908fE5eaA5505af85C10911a718FD"
