7dcbdd8062a1572fb96e1cbe34060a28ff28b2bb
adnpark <aidenp.dev@gmail.com>
2024-03-07 17:55:53 +0900

    feat: update contract address and fix dummy signature

--- plugins/passkey/index.ts
+++ plugins/passkey/index.ts
@@ -7,4 +7,4 @@ import {
 export { createPasskeyValidator, getPasskeyValidator, type KernelValidator }
 
 export const WEBAUTHN_VALIDATOR_ADDRESS =
-    "0x8a3681d056e5772da8cEBe98549b7f0439Fba47A"
+    "0x3a0980D497F908fE5eaA5505af85C10911a718FD"
--- plugins/passkey/toPasskeyValidator.ts
+++ plugins/passkey/toPasskeyValidator.ts
@@ -314,11 +314,11 @@ export async function createPasskeyValidator<
                     { name: "s", type: "uint256" }
                 ],
                 [
-                    "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-                    '{"type":"webauthn.get","challenge":"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","origin":"https://example.com"}',
-                    maxUint256,
-                    11111111111111111111111111111111111111111111111111111111111111111111111111111n,
-                    22222222222222222222222222222222222222222222222222222222222222222222222222222n
+                    "0x49960de5880e8c687434170f6476605b8fe4aeb9a28632c7995cf3ba831d97631d00000000",
+                    '{"type":"webauthn.get","challenge":"tbxXNFS9X_4Byr1cMwqKrIGB-_30a0QhZ6y7ucM0BOE","origin":"http://localhost:3000","crossOrigin":false}',
+                    1n,
+                    44941127272049826721201904734628716258498742255959991581049806490182030242267n,
+                    9910254599581058084911561569808925251374718953855182016200087235935345969636n
                 ]
             )
             return encodedSignature
