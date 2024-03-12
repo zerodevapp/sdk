8c9b067a2f14330675e470f8d82c139abe4b4402
adnpark <aidenp.dev@gmail.com>
2024-03-12 10:50:41 +0900

    feat: update weighted ecdsa validator and bump version

--- plugins/weighted-ecdsa/index.ts
+++ plugins/weighted-ecdsa/index.ts
@@ -1,15 +1,15 @@
 import type { KernelValidator } from "@zerodev/sdk/types"
 import {
-    createWeightedECDSAValidator,
-    getUpdateConfigCall
+  createWeightedECDSAValidator,
+  getUpdateConfigCall
 } from "./toWeightedECDSAValidatorPlugin.js"
 
 export {
-    createWeightedECDSAValidator,
-    getUpdateConfigCall,
-    type KernelValidator
+  createWeightedECDSAValidator,
+  getUpdateConfigCall,
+  type KernelValidator
 }
 
 /// @dev note that only deployed on polygon-mumbai now
 export const WEIGHTED_ECDSA_VALIDATOR_ADDRESS =
-    "0x01D30994b3682dF8124e067a92a0B7E8B14E9BBf"
+  "0xf230ac0AD98F81cd9806b5A6B16f8Fb92Fb6Fc48"
