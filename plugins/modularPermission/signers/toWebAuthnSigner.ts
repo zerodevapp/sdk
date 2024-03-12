95462e820066e5b3380080b4738f1192867bb934
adnpark <aidenp.dev@gmail.com>
2024-03-12 16:05:24 +0900

    fix: fix dummy signature for webAuthn signer and bump version

--- plugins/modularPermission/signers/toWebAuthnSigner.ts
+++ plugins/modularPermission/signers/toWebAuthnSigner.ts
@@ -11,7 +11,6 @@ import {
     type TypedDataDefinition,
     getTypesForEIP712Domain,
     hashTypedData,
-    maxUint256,
     validateTypedData
 } from "viem"
 import { type SignableMessage, encodeAbiParameters } from "viem"
@@ -219,12 +218,12 @@ export const toWebAuthnSigner = async <
                     { name: "usePrecompiled", type: "bool" }
                 ],
                 [
-                    "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-                    '{"type":"webauthn.get","challenge":"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","origin":"https://example.com"}',
-                    maxUint256,
-                    maxUint256,
-                    11111111111111111111111111111111111111111111111111111111111111111111111111111n,
-                    22222222222222222222222222222222222222222222222222222222222222222222222222222n,
+                    "0x49960de5880e8c687434170f6476605b8fe4aeb9a28632c7995cf3ba831d97631d00000000",
+                    '{"type":"webauthn.get","challenge":"tbxXNFS9X_4Byr1cMwqKrIGB-_30a0QhZ6y7ucM0BOE","origin":"http://localhost:3000","crossOrigin":false}',
+                    23n,
+                    1n,
+                    44941127272049826721201904734628716258498742255959991581049806490182030242267n,
+                    9910254599581058084911561569808925251374718953855182016200087235935345969636n,
                     false
                 ]
             )
