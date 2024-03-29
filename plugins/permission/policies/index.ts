7009dee7ffbc1615eebbbbde94ec5e95e7ccdd60
adnpark <aidenp.dev@gmail.com>
2024-03-29 18:03:36 +0900

    feat: remove sudo policy

--- plugins/permission/policies/index.ts
+++ plugins/permission/policies/index.ts
@@ -1,5 +1,4 @@
 export { toGasPolicy, type GasPolicyParams } from "./toGasPolicy.js"
-export { toSudoPolicy } from "./toSudoPolicy.js"
 export {
     toSignatureCallerPolicy,
     type SignaturePolicyParams
