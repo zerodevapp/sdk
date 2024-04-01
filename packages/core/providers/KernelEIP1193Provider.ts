44976ef49fd664727b88e53a97d1ee0eef8c8626
Blake Duncan <blakecduncan@gmail.com>
2024-04-01 14:17:58 -0400

    feat: add eth_sign and eth_signTypedData to kernel provider

--- packages/core/providers/KernelEIP1193Provider.ts
+++ packages/core/providers/KernelEIP1193Provider.ts
@@ -26,8 +26,11 @@ export class KernelEIP1193Provider extends EventEmitter {
                 return this.handleEthAccounts()
             case "eth_sendTransaction":
                 return this.handleEthSendTransaction(params)
+            case "eth_sign":
+                return this.handleEthSign(params as [string, string])
             case "personal_sign":
                 return this.handlePersonalSign(params as [string, string])
+            case "eth_signTypedData":
             case "eth_signTypedData_v4":
                 return this.handleEthSignTypedDataV4(params as [string, string])
             default:
@@ -52,6 +55,26 @@ export class KernelEIP1193Provider extends EventEmitter {
     private async handleEthSendTransaction(params: unknown): Promise<Hash> {
         const [tx] = params as [SendTransactionParameters]
         return this.kernelClient.sendTransaction(tx)
+    }
+
+    private async handleEthSign(params: [string, string]): Promise<string> {
+        if (!this.kernelClient?.account) {
+            throw new Error("account not connected!")
+        }
+        const [address, message] = params
+        if (
+            address.toLowerCase() !==
+            this.kernelClient.account.address.toLowerCase()
+        ) {
+            throw new Error(
+                "cannot sign for address that is not the current account"
+            )
+        }
+
+        return this.kernelClient.signMessage({
+            message,
+            account: this.kernelClient.account
+        })
     }
 
     private async handlePersonalSign(
