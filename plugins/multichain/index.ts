66ea4ebdab4ba1c0fcf8c5dfd8487d37d9918a25
adnpark <aidenp.dev@gmail.com>
2024-05-07 20:30:55 +0900

    feat: add support for enabling regular validators in multi chain plugin

--- plugins/multichain/index.ts
+++ plugins/multichain/index.ts
@@ -1,6 +1,12 @@
 import type { KernelValidator } from "@zerodev/sdk/types"
 import { signUserOps } from "./signUserOps.js"
+import { signUserOpsWithEnable } from "./signUserOpsWithEnable.js"
 import { toMultiChainValidator } from "./toMultiChainValidator.js"
 
-export { toMultiChainValidator, signUserOps, type KernelValidator }
+export {
+    toMultiChainValidator,
+    signUserOps,
+    signUserOpsWithEnable,
+    type KernelValidator
+}
 export * from "./constants.js"
---
+++ plugins/multichain/signUserOpsWithEnable.ts
@@ -0,0 +1,114 @@
+import {
+    type Action,
+    type KernelSmartAccount,
+    KernelV3AccountAbi,
+    getEncodedPluginsData
+} from "@zerodev/sdk"
+import MerkleTree from "merkletreejs"
+import type { UserOperation } from "permissionless"
+import type {
+    EntryPoint,
+    GetEntryPointVersion
+} from "permissionless/types/entrypoint"
+import {
+    type Hex,
+    concatHex,
+    encodeAbiParameters,
+    getAbiItem,
+    hashTypedData,
+    keccak256,
+    toFunctionSelector,
+    zeroAddress
+} from "viem"
+
+export type MultiChainUserOpConfigForEnable<entryPoint extends EntryPoint> = {
+    account: KernelSmartAccount<EntryPoint>
+    userOp: UserOperation<GetEntryPointVersion<entryPoint>>
+}
+
+/**
+ *
+ * @dev Sign user operations with enable signatures for multi-chain validator
+ * @returns Signed user operations
+ */
+export const signUserOpsWithEnable = async ({
+    multiChainUserOpConfigs
+}: {
+    multiChainUserOpConfigs: MultiChainUserOpConfigForEnable<EntryPoint>[]
+}): Promise<UserOperation<GetEntryPointVersion<EntryPoint>>[]> => {
+    const pluginEnableTypedDatas = await Promise.all(
+        multiChainUserOpConfigs.map(async (config) => {
+            return config.account.kernelPluginManager.getPluginsEnableTypedData(
+                config.account.address
+            )
+        })
+    )
+
+    const leaves = pluginEnableTypedDatas.map((typedData) => {
+        return hashTypedData(typedData)
+    })
+
+    const merkleTree = new MerkleTree(leaves, keccak256, {
+        sortPairs: true
+    })
+
+    const merkleRoot = merkleTree.getHexRoot() as Hex
+
+    const ecdsaSig =
+        await multiChainUserOpConfigs[0].account.kernelPluginManager.sudoValidator?.signMessage(
+            {
+                message: {
+                    raw: merkleRoot
+                }
+            }
+        )
+
+    if (!ecdsaSig) {
+        throw new Error(
+            "No ecdsaSig, check if the sudo validator is multi-chain validator"
+        )
+    }
+
+    const enableSigs = multiChainUserOpConfigs.map((_, index) => {
+        const merkleProof = merkleTree.getHexProof(leaves[index]) as Hex[]
+        const encodedMerkleProof = encodeAbiParameters(
+            [{ name: "proof", type: "bytes32[]" }],
+            [merkleProof]
+        )
+        return concatHex([ecdsaSig, merkleRoot, encodedMerkleProof])
+    })
+
+    const userOpSignatures = await Promise.all(
+        multiChainUserOpConfigs.map(async (config) => {
+            return config.account.kernelPluginManager.signUserOperationWithActiveValidator(
+                config.userOp
+            )
+        })
+    )
+
+    const action: Action = {
+        selector: toFunctionSelector(
+            getAbiItem({ abi: KernelV3AccountAbi, name: "execute" })
+        ),
+        address: zeroAddress
+    }
+
+    const finalSignatures = await Promise.all(
+        multiChainUserOpConfigs.map(async (config, index) => {
+            return await getEncodedPluginsData({
+                accountAddress: config.account.address,
+                enableSignature: enableSigs[index],
+                userOpSignature: userOpSignatures[index],
+                action,
+                validator: config.account.kernelPluginManager.activeValidator
+            })
+        })
+    )
+
+    return multiChainUserOpConfigs.map((config, index) => {
+        return {
+            ...config.userOp,
+            signature: finalSignatures[index]
+        }
+    })
+}
