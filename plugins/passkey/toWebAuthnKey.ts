ddc4031245b73a0a2b8fedc401cba65424d5c70b
adnpark <aidenp.dev@gmail.com>
2024-06-03 23:02:38 +0900

    refactor: decouple creating passkey and the validator

---
+++ plugins/passkey/toWebAuthnKey.ts
@@ -0,0 +1,161 @@
+import { type Hex, keccak256 } from "viem"
+import { b64ToBytes, uint8ArrayToHexString } from "./utils.js"
+
+export enum WebAuthnMode {
+    Register = "register",
+    Login = "login"
+}
+
+export type WebAuthnKey = {
+    pubX: bigint
+    pubY: bigint
+    authenticatorIdHash: Hex
+}
+
+export type WebAuthnAccountParams = {
+    passkeyName: string
+    passkeyServerUrl: string
+    webAuthnKey?: WebAuthnKey
+    mode?: WebAuthnMode
+}
+
+export const toWebAuthnKey = async ({
+    passkeyName,
+    passkeyServerUrl,
+    webAuthnKey,
+    mode = WebAuthnMode.Register
+}: WebAuthnAccountParams): Promise<WebAuthnKey> => {
+    if (webAuthnKey) {
+        return webAuthnKey
+    }
+    let pubKey: string | undefined
+    let authenticatorIdHash: Hex
+    if (mode === WebAuthnMode.Login) {
+        // Get login options
+        const loginOptionsResponse = await fetch(
+            `${passkeyServerUrl}/login/options`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                credentials: "include"
+            }
+        )
+        const loginOptions = await loginOptionsResponse.json()
+
+        // Start authentication (login)
+        const { startAuthentication } = await import("@simplewebauthn/browser")
+        const loginCred = await startAuthentication(loginOptions)
+
+        // get authenticatorIdHash
+        authenticatorIdHash = keccak256(
+            uint8ArrayToHexString(b64ToBytes(loginCred.id))
+        )
+
+        // Verify authentication
+        const loginVerifyResponse = await fetch(
+            `${passkeyServerUrl}/login/verify`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ cred: loginCred }),
+                credentials: "include"
+            }
+        )
+
+        const loginVerifyResult = await loginVerifyResponse.json()
+
+        if (window.sessionStorage === undefined) {
+            throw new Error("sessionStorage is not available")
+        }
+        sessionStorage.setItem("userId", loginVerifyResult.userId)
+
+        if (!loginVerifyResult.verification.verified) {
+            throw new Error("Login not verified")
+        }
+        // Import the key
+        pubKey = loginVerifyResult.pubkey // Uint8Array pubkey
+    } else {
+        // Get registration options
+        const registerOptionsResponse = await fetch(
+            `${passkeyServerUrl}/register/options`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ username: passkeyName }),
+                credentials: "include"
+            }
+        )
+        const registerOptions = await registerOptionsResponse.json()
+
+        // save userId to sessionStorage
+        if (window.sessionStorage === undefined) {
+            throw new Error("sessionStorage is not available")
+        }
+        sessionStorage.setItem("userId", registerOptions.userId)
+
+        // Start registration
+        const { startRegistration } = await import("@simplewebauthn/browser")
+        const registerCred = await startRegistration(registerOptions.options)
+
+        // get authenticatorIdHash
+        authenticatorIdHash = keccak256(
+            uint8ArrayToHexString(b64ToBytes(registerCred.id))
+        )
+
+        // Verify registration
+        const registerVerifyResponse = await fetch(
+            `${passkeyServerUrl}/register/verify`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    userId: registerOptions.userId,
+                    username: passkeyName,
+                    cred: registerCred
+                }),
+                credentials: "include"
+            }
+        )
+
+        const registerVerifyResult = await registerVerifyResponse.json()
+        if (!registerVerifyResult.verified) {
+            throw new Error("Registration not verified")
+        }
+
+        // Import the key
+        pubKey = registerCred.response.publicKey
+    }
+
+    if (!pubKey) {
+        throw new Error("No public key returned from registration credential")
+    }
+    const spkiDer = Buffer.from(pubKey, "base64")
+    const key = await crypto.subtle.importKey(
+        "spki",
+        spkiDer,
+        {
+            name: "ECDSA",
+            namedCurve: "P-256"
+        },
+        true,
+        ["verify"]
+    )
+
+    // Export the key to the raw format
+    const rawKey = await crypto.subtle.exportKey("raw", key)
+    const rawKeyBuffer = Buffer.from(rawKey)
+
+    // The first byte is 0x04 (uncompressed), followed by x and y coordinates (32 bytes each for P-256)
+    const pubKeyX = rawKeyBuffer.subarray(1, 33).toString("hex")
+    const pubKeyY = rawKeyBuffer.subarray(33).toString("hex")
+
+    return {
+        pubX: BigInt(`0x${pubKeyX}`),
+        pubY: BigInt(`0x${pubKeyY}`),
+        authenticatorIdHash
+    }
+}
