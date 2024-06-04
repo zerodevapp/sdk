f291fde8ec25af33a091c17b42843643c6586b22
adnpark <aidenp.dev@gmail.com>
2024-06-04 16:53:29 +0900

    feat: cache enable signatures

--- packages/core/accounts/utils/toKernelPluginManager.ts
+++ packages/core/accounts/utils/toKernelPluginManager.ts
@@ -100,8 +100,9 @@ export async function toKernelPluginManager<
                     return ValidatorMode.plugin
                 }
 
-                const enableSignature =
-                    await getPluginEnableSignature(accountAddress)
+                const enableSignature = await getPluginEnableSignature(
+                    accountAddress
+                )
                 if (!enableSignature) {
                     throw new Error("Enable signature not set")
                 }
@@ -125,8 +126,9 @@ export async function toKernelPluginManager<
             if (await isPluginEnabled(accountAddress, action.selector)) {
                 return userOpSignature
             }
-            const enableSignature =
-                await getPluginEnableSignature(accountAddress)
+            const enableSignature = await getPluginEnableSignature(
+                accountAddress
+            )
             return getEncodedPluginsDataV2({
                 enableSignature,
                 userOpSignature,
@@ -197,6 +199,7 @@ export async function toKernelPluginManager<
             validatorNonce
         })
         ownerSig = await sudo.signTypedData(typedData)
+        pluginEnableSignature = ownerSig
 
         return ownerSig
     }
@@ -262,8 +265,9 @@ export async function toKernelPluginManager<
             throw new Error("EntryPoint v0.7 not supported yet")
         },
         signUserOperation: async (userOperation) => {
-            const userOpSig =
-                await activeValidator.signUserOperation(userOperation)
+            const userOpSig = await activeValidator.signUserOperation(
+                userOperation
+            )
             if (entryPointVersion === "v0.6") {
                 return concatHex([
                     await getSignatureData(
@@ -290,8 +294,9 @@ export async function toKernelPluginManager<
             validUntil
         }),
         getDummySignature: async (userOperation) => {
-            const userOpSig =
-                await activeValidator.getDummySignature(userOperation)
+            const userOpSig = await activeValidator.getDummySignature(
+                userOperation
+            )
             if (entryPointVersion === "v0.6") {
                 return concatHex([
                     await getSignatureData(
