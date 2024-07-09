65a226ccb1b401655c871ca2d09f11841e1ce437
Blake Duncan <blakecduncan@gmail.com>
2024-07-09 12:36:00 -0400

    chore: add polygon amoy support

--- packages/accounts/src/kernel-zerodev/utils/custom-chains.ts
+++ packages/accounts/src/kernel-zerodev/utils/custom-chains.ts
@@ -265,3 +265,31 @@ export const degen = defineChain({
     },
   },
 })
+
+export const polygonAmoy = defineChain({
+  id: 80_002,
+  name: 'Polygon Amoy',
+  network: 'polygon-amoy',
+  nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-amoy.polygon.technology'],
+    },
+    public: {
+      http: ['https://rpc-amoy.polygon.technology'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'OK LINK',
+      url: 'https://www.oklink.com/amoy',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 3127388,
+    },
+  },
+  testnet: true,
+})
