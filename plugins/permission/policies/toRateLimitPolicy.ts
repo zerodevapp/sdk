7fe571c90330b581ab9e01861f028869062803f4
leekt <leekt216@gmail.com>
2025-03-11 19:32:53 +0900

    todo: need to run test

--- plugins/permission/policies/toRateLimitPolicy.ts
+++ plugins/permission/policies/toRateLimitPolicy.ts
@@ -1,6 +1,6 @@
 import { concatHex } from "viem"
 import { PolicyFlags } from "../constants.js"
-import { RATE_LIMIT_POLICY_CONTRACT } from "../constants.js"
+import { RATE_LIMIT_POLICY_CONTRACT, RATE_LIMIT_POLICY_WITH_RESET_CONTRACT } from "../constants.js"
 import type { Policy, PolicyParams } from "../types.js"
 
 export type RateLimitPolicyParams = PolicyParams & {
@@ -18,6 +18,7 @@ export function toRateLimitPolicy({
 }: RateLimitPolicyParams): Policy {
     return {
         getPolicyData: () => {
+          if(policyAddress === RATE_LIMIT_POLICY_CONTRACT) {
             const intervalHex = interval.toString(16).padStart(12, "0")
             const countHex = count.toString(16).padStart(12, "0")
             const startAtHex = startAt.toString(16).padStart(12, "0")
@@ -25,6 +26,19 @@ export function toRateLimitPolicy({
             const data = intervalHex + countHex + startAtHex
 
             return `0x${data}`
+          } else if(policyAddress === RATE_LIMIT_POLICY_WITH_RESET_CONTRACT) {
+            if(startAt !== 0 && startAt !== undefined) {
+              throw new Error("RATE_LIMIT_POLICY_WITH_RESET_CONTRACT does not support startAt")
+            }
+            const intervalHex = interval.toString(16).padStart(12, "0")
+            const countHex = count.toString(16).padStart(12, "0")
+
+            const data = intervalHex + countHex
+
+            return `0x${data}`
+          } else {
+            throw new Error("Invalid policy address")
+          }
         },
         getPolicyInfoInBytes: () => {
             return concatHex([policyFlag, policyAddress])
