f92b41f5c1342bb17b7a0849abccbd802f2dfb65
Sahil Vasava <sahilvasava@gmail.com>
2025-05-07 19:47:10 +0900

    giza validator plugin demo and test file

---
+++ plugins/giza/constants.ts
@@ -0,0 +1,7 @@
+import type { Address } from "viem"
+
+export const kernelVersionRangeToValidator: {
+    [key: string]: Address
+} = {
+    ">=0.3.0": "0x4513b6b7c355E5B8a9F5b38B1720B51800701C29"
+}
---
+++ plugins/giza/index.ts
@@ -0,0 +1,13 @@
+import type { KernelValidator } from "@zerodev/sdk/types"
+import {
+    getValidatorAddress,
+    signerToGizaValidator
+} from "./toGizaValidatorPlugin.js"
+
+export {
+    getValidatorAddress,
+    signerToGizaValidator,
+    type KernelValidator,
+}
+export * from "./constants.js"
+
