f05ac630ce5fd2365b083d5a4dd412abba018795
leekt <leekt216@gmail.com>
2025-11-21 15:24:27 +0900

    lint

--- packages/test/v0.7/weightedECDSAValidator.test.ts
+++ packages/test/v0.7/weightedECDSAValidator.test.ts
@@ -1,17 +1,24 @@
 // @ts-expect-error
 import { beforeAll, describe, expect, test } from "bun:test"
+import { verifyMessage } from "@ambire/signature-validator"
 import type {
     KernelAccountClient,
     KernelSmartAccountImplementation,
     ZeroDevPaymasterClient
 } from "@zerodev/sdk"
+import { EIP1271Abi } from "@zerodev/sdk"
+import { ethers } from "ethers"
 import {
     type Chain,
     type PublicClient,
     type Transport,
+    hashMessage,
+    hashTypedData,
     zeroAddress
 } from "viem"
 import type { SmartAccount } from "viem/account-abstraction"
+import { sepolia } from "viem/chains"
+import { config } from "../config.js"
 import {
     getEntryPoint,
     getKernelAccountClient,
@@ -19,11 +26,8 @@ import {
     getSignersToWeightedEcdsaKernelAccount,
     getZeroDevPaymasterClient
 } from "./utils"
-
 const ETHEREUM_ADDRESS_LENGTH = 42
 const ETHEREUM_ADDRESS_REGEX = /^0x[0-9a-fA-F]{40}$/
-const SIGNATURE_LENGTH = 174
-const SIGNATURE_REGEX = /^0x[0-9a-fA-F]{172}$/
 const TX_HASH_LENGTH = 66
 const TX_HASH_REGEX = /^0x[0-9a-fA-F]{64}$/
 const TEST_TIMEOUT = 1000000
@@ -55,6 +59,100 @@ describe("Weighted ECDSA kernel Account", () => {
         expect(account.address).toMatch(ETHEREUM_ADDRESS_REGEX)
         expect(account.address).not.toEqual(zeroAddress)
     })
+
+    test(
+        "Client signMessage should return a valid signature",
+        async () => {
+            // to make sure kernel is deployed
+            const tx = await kernelClient.sendTransaction({
+                calls: [
+                    {
+                        to: zeroAddress,
+                        value: 0n,
+                        data: "0x"
+                    }
+                ]
+            })
+            console.log("tx", tx)
+
+            const message = "hello world"
+            const response = await kernelClient.signMessage({
+                message
+            })
+            console.log("hashMessage(message)", hashMessage(message))
+            console.log("response", response)
+            const ambireResult = await verifyMessage({
+                signer: account.address,
+                message,
+                signature: response,
+                provider: new ethers.providers.JsonRpcProvider(
+                    config["0.7"][sepolia.id].rpcUrl
+                )
+            })
+            expect(ambireResult).toBeTrue()
+
+            const eip1271response = await publicClient.readContract({
+                address: account.address,
+                abi: EIP1271Abi,
+                functionName: "isValidSignature",
+                args: [hashMessage(message), response]
+            })
+            console.log("eip1271response", eip1271response)
+            console.log("response", response)
+            expect(eip1271response).toEqual("0x1626ba7e")
+            expect(response).toBeString()
+        },
+        TEST_TIMEOUT
+    )
+
+    test(
+        "Smart account client signTypedData",
+        async () => {
+            const domain = {
+                chainId: 1,
+                name: "Test",
+                verifyingContract: zeroAddress
+            }
+
+            const primaryType = "Test"
+
+            const types = {
+                Test: [
+                    {
+                        name: "test",
+                        type: "string"
+                    }
+                ]
+            }
+
+            const message = {
+                test: "hello world"
+            }
+            const typedHash = hashTypedData({
+                domain,
+                primaryType,
+                types,
+                message
+            })
+
+            const response = await kernelClient.signTypedData({
+                domain,
+                primaryType,
+                types,
+                message
+            })
+
+            const eip1271response = await publicClient.readContract({
+                address: account.address,
+                abi: EIP1271Abi,
+                functionName: "isValidSignature",
+                args: [typedHash, response]
+            })
+            expect(eip1271response).toEqual("0x1626ba7e")
+            expect(response).toBeString()
+        },
+        TEST_TIMEOUT
+    )
 
     test(
         "Smart account client send transaction",
